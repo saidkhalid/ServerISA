6d854bf7be1d3289964640204b806f98888d7e5f
Ihab EL-KASHEF <ihab.el-kashef@etudiant.univ-lille1.fr>
2015-01-28 11:55:33 +0100

    cnx réussiee | messages clients

--- webserver/socket.c
+++ webserver/socket.c
@@ -10,27 +10,33 @@
 
 #include "socket.h"
 
-void server(){
-	int socket_server ;
+int creer_serveur(int port){
+	int socket_serveur ;
 	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
 	if(socket_serveur == -1){
-		perror ( " socket_serveur " );
+		perror (" socket_serveur ");
+		return -1;
 		/* traitement de l ’ erreur */
 	}
+
 	/* Utilisation de la socket serveur */
 	struct sockaddr_in saddr ;
 	saddr.sin_family = AF_INET; /* Socket ipv4 */
-	saddr.sin_port = htons(8080); /* Port d ’ écoute */
+	saddr.sin_port = htons(port); /* Port d ’ écoute */
 	saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
 
 	if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1){
 		perror ( " bind socker_serveur " );
+		return -1;
 		/* traitement de l ’ erreur */
 	}
 	
-	if ( listen ( socket_serveur , 10) == -1){
+	if (listen(socket_serveur, 10) == -1){
 		perror ( " listen socket_serveur " );
+		return -1;
 		/* traitement d ’ erreur */
 	}
+	
+	return socket_serveur;	
 }
