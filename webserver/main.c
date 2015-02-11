cb7daefeb18398bfdca713ecd1f8fa32cd82815a
saidkhalid <saidkhalid1996@gmail.com>
2015-02-11 10:24:31 +0100

    ajout des fichiers sources

---
+++ webserver/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include "socket.h" 
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main (){
+
+
+/* Arnold Robbins in the LJ of February ’95 , describing RCS */
+
+	int socket_client ;
+	int socket_serveur = creer_serveur(8080);
+	int ret;
+	char message[256];
+
+	while(1){
+		socket_client = accept(socket_serveur, NULL, NULL);
+
+		if (socket_client == -1){
+			perror ( " accept " );
+			/* traitement d ’ erreur */
+		}
+		/* On peut maintenant dialoguer avec le client */
+	
+		const char * message_bienvenue = " Bonjour, bienvenue sur mon serveur.\n" ;
+		int i;
+		for (i=1 ; i<11; i++){
+			if (write(socket_client, message_bienvenue, strlen(message_bienvenue))==-1){
+				perror("write");
+			}
+		}
+
+		while ((ret = read(socket_client, message, 256))>0){ 		
+			if (write(socket_client, message, ret) == -1){
+				perror("write");
+			}
+		}
+	}
+}
