bed95b068196b9f602263ba870abf6d97dde247b
Ihab El Kashef <Baz@MacBook-Pro-de-Ihab-El-Kashef.local>
2015-03-28 14:15:00 +0100

    url _ or_404_clean

--- webserver/main.c
+++ webserver/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include "socket.h"
@@ -13,163 +14,173 @@
 #include <signal.h>
 
 enum http_method {
-
-	HTTP_GET ,
-	HTTP_UNSUPPORTED ,
-	};
+    
+    HTTP_GET ,
+    HTTP_UNSUPPORTED ,
+};
 
 typedef struct
 {
-	enum http_method method ;
-	int major_version ;
-	int minor_version ;
-	char * url ;
+    enum http_method method ;
+    int major_version ;
+    int minor_version ;
+    char * url ;
 } http_request ;
 
-void skip_headers(FILE *client){
-	char message[1024];
-	if(strlen(client)<2){
-		fgets(client,1024,message);
-	}	
-}
+/*void skip_headers(FILE *client){
+    char message[1024];
+    if(strlen(client)<2){
+        fgets(client,1024,message);
+    }
+}*/
 void send_status(FILE *client , int code , const char *reason_phrase){
-	//code=404;
-	char http[256];
-	//reason_phrase="NOT FOUND \n";
-	int m=1,M=1;
-	
-	fprintf(client,"%s HTTP/%d.%d %d %s",http,M,m,code,reason_phrase);
+    //code=404;
+    char http[256];
+    //reason_phrase="NOT FOUND \n";
+    int m=1,M=1;
+    
+    fprintf(client,"%s HTTP/%d.%d %d %s",http,M,m,code,reason_phrase);
 }
 
 void send_response(FILE *client , int code , const char *reason_phrase ,const char *message_body){
-
-	send_status(client,code,reason_phrase);
-	
+    
+    send_status(client,code,reason_phrase);
+    if(fprintf(client, "Connection: close\r\nContent-Length: %d\r\nContent-Type: text/plain\r\n\r\n%s",(int)strlen(message_body),message_body)<0){
+        perror("error send_response");
+    }
+    
 }
 
 int parse_http_request ( const char * request_line , http_request * request ){
-	char slash[256];
-	char get[3]="GET";
-	char url[256],http[256];
-	int M,m;
-	/*
-	* Vérifie si le premier mot de la requete est bien "GET"
-	*/
-	printf(request_line);
-	if((request_line[0]==get[0]) && (request_line[1]==get[1]) && (request_line[2]==get[2])){
-		//perror("ligne");
-		printf("GET ok \n");
-		request->method = HTTP_GET;
-		/*	
-		* Stocke la requete dans les variables définies plus haut et vérifie que le contenu de M et m est le bon.
-		*/
-		sscanf(request_line, "%s %s HTTP/%d.%d %s",url,http,&M,&m,slash);
-		if((M==1) || (m==0 && m==1)){
-		// printf("erreur de saisie \n");
-		request->minor_version = m;
-		request->major_version = M;
-		/*
-		* La requete est valide que si la forme de l'entete est vérifié (conditions précédentes) et le premier caractère qui le suive est '/'
-		*/
-
-		if (slash[0]=='/') {
-		//if (message[0] == '\n' || (message[0] == '\r' && message[1] == '\n')) {
-		//fprintf(fd, "%s", "message is vide");
-			return 1;
-		}
-	}
-	}
-	
-	return 0;
+    char slash[256];
+    char get[3]="GET";
+    char url[256],http[256];
+    int M,m;
+    /*
+     * Vérifie si le premier mot de la requete est bien "GET"
+     */
+    printf("%s",request_line);
+    if((request_line[0]==get[0]) && (request_line[1]==get[1]) && (request_line[2]==get[2])){
+        //perror("ligne");
+        printf("GET ok \n");
+        request->method = HTTP_GET;
+        /*
+         * Stocke la requete dans les variables définies plus haut et vérifie que le contenu de M et m est le bon.
+         */
+        sscanf(request_line, "%s %s HTTP/%d.%d %s",url,http,&M,&m,slash);
+        if((M==1) || (m==0 && m==1)){
+            // printf("erreur de saisie \n");
+            request->minor_version = m;
+            request->major_version = M;
+            /*
+             * La requete est valide que si la forme de l'entete est vérifié (conditions précédentes) et le premier caractère qui le suive est '/'
+             */
+            
+            if (slash[0]=='/') {
+                //if (message[0] == '\n' || (message[0] == '\r' && message[1] == '\n')) {
+                //fprintf(fd, "%s", "message is vide");
+                return 1;
+            }
+        }
+    }
+    
+    return 0;
 }
 
 char * fgets_or_exit ( char * message , int size , FILE * fd ){
-	/*
-	* si le client est deconnecté on interromp le processus
-	*/
-		if(fd==NULL){
-		perror("fgets");
-		exit(1);
-	}
-	
-	/*
-	* si le client est connecté on fait un appel à fgets
-	*/
-		return fgets(message,size,fd);
-		/*printf("%s",url);
-		fclose(fd);
-		exit(1);*/
+    /*
+     * si le client est deconnecté on interromp le processus
+     */
+    if(fd==NULL){
+        perror("fgets");
+        exit(1);
+    }
+    
+    /*
+     * si le client est connecté on fait un appel à fgets
+     */
+    return fgets(message,size,fd);
+    /*printf("%s",url);
+     fclose(fd);
+     exit(1);*/
 }
 
 /*void skip_headers(FILE *client){
 	fgets_or_exit(
-}*/
+ }*/
 
 
-	
+
 int main (){
-	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	int socket_client;
-	int socket_serveur = creer_serveur(8080);
-	int SIZE=256;
-	char message[SIZE];
-	char messageOk[256] = "HTTP/1.1 200 OK\nConnection: close\nContent-Length: 17\n";
-	char messageError[256] = "HTTP/1.1 400 Bad Request\nConnection: close\nContent-Length: 17\n400 Bad request\n" ;
-	int pid;
-	http_request request; //la fameuse structure qui sera modifier par parse_http_request
-	request.method = 0;
-	request.major_version = 0 ;
-	request.minor_version = 0 ;
-	request.url = 0;
-	
-	if(socket_serveur==-1){
-		return 1;
-	}
-	
-	while(1){
-		socket_client = accept(socket_serveur,NULL,NULL);
-		if (socket_client == -1){
-		perror ( " accept xd " );
-		continue;
-	/* traitement d ’ erreur */
-	}
-	
-	FILE * fd=fdopen(socket_client,"w+");
-	if(fd ==NULL){
-		printf("erreurr");
-	}
-	pid=fork();
-	if(pid==0){
-		if(parse_http_request(fgets_or_exit(message,SIZE,fd), &request)==1){
-			fprintf(fd, "%s", messageOk);
-		}else{
-			fprintf(fd, "%s", messageError);
-		}
-		fflush(fd);
-		
-		
-	/* On peut maintenant dialoguer avec le client */
-	/*const char * message_bienvenue = " Bonjour, bienvenue sur mon serveur.\n," ;
-	int i;
-	for (i=1 ; i<11; i++){
-	if (write(socket_client, message_bienvenue, strlen(message_bienvenue))==-1){
-	perror("write");
-	}
-	}
-	while ((ret = read(socket_client, message, 256))>0){
-	if (write(socket_client, message, ret) == -1){
-	perror("write");
-	}
-	}*/
-	fclose(fd);
-	exit(0);
-	}else{
-		fclose(fd);
-	}
-	}
-	initialiser_signaux();
-	close(socket_client);
+    /* Arnold Robbins in the LJ of February ’95 , describing RCS */
+    int socket_client;
+    int socket_serveur = creer_serveur(8080);
+    int SIZE=256;
+    char message[SIZE];
+    char messageOk[256] = "HTTP/1.1 200 OK\nConnection: close\nContent-Length: 17\n";
+    char messageError[256] = "HTTP/1.1 400 Bad Request\nConnection: close\nContent-Length: 17\n400 Bad request\n" ;
+    int pid;
+    http_request request; //la fameuse structure qui sera modifier par parse_http_request
+    request.method = 0;
+    request.major_version = 0 ;
+    request.minor_version = 0 ;
+    request.url = 0;
+    
+    if(socket_serveur==-1){
+        return 1;
+    }
+    
+    while(1){
+        socket_client = accept(socket_serveur,NULL,NULL);
+        if (socket_client == -1){
+            perror ( " accept xd " );
+            continue;
+            /* traitement d ’ erreur */
+        }
+        
+        FILE * fd=fdopen(socket_client,"w+");
+        if(fd ==NULL){
+            printf("erreurr");
+        }
+        pid=fork();
+        if(pid==0){
+            if(parse_http_request(fgets_or_exit(message,SIZE,fd), &request)==1){
+                fprintf(fd, "%s", messageOk);
+                send_response(fd,200,"OK"," OK \r\n");
+            }else{
+                fprintf(fd, "%s", messageError);
+                send_response(fd , 400 , "Bad Request" , " Bad request \r\n" );
 
-  return 0;
+            }
+        
+            
+            
+            fflush(fd);
+            
+            
+            
+            /* On peut maintenant dialoguer avec le client */
+            /*const char * message_bienvenue = " Bonjour, bienvenue sur mon serveur.\n," ;
+             int i;
+             for (i=1 ; i<11; i++){
+             if (write(socket_client, message_bienvenue, strlen(message_bienvenue))==-1){
+             perror("write");
+             }
+             }
+             while ((ret = read(socket_client, message, 256))>0){
+             if (write(socket_client, message, ret) == -1){
+             perror("write");
+             }
+             }*/
+            fclose(fd);
+            exit(0);
+        }else{
+            fclose(fd);
+        }
+    }
+    initialiser_signaux();
+    close(socket_client);
+    
+    return 0;
 }
 
